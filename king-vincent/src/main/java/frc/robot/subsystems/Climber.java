5b22272d0d39d9bde2b0a229104d3ea9ac901405
Raphael Hoang <fluffier913@gmail.com>
2024-10-16 17:07:33 -0400

    kk

--- king-vincent/src/main/java/frc/robot/subsystems/Climber.java
+++ king-vincent/src/main/java/frc/robot/subsystems/Climber.java
@@ -27,6 +27,15 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+
+
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+
+
 public class Climber extends SubsystemBase {
 
   public CANSparkMax motorLeft;
