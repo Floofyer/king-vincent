1621958b4d3e8d8d39887ca20d659c3451aa9dac
Raphael Hoang <fluffier913@gmail.com>
2024-10-16 17:14:42 -0400

    mk

--- king-vincent/src/main/java/frc/robot/subsystems/Climber.java
+++ king-vincent/src/main/java/frc/robot/subsystems/Climber.java
@@ -34,6 +34,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 
 public class Climber extends SubsystemBase {
