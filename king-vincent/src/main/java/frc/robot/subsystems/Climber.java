c6e99ebe224eaca7dbf0b4e7c236ddc61a349368
Raphael Hoang <fluffier913@gmail.com>
2024-10-18 15:41:26 -0400

    when will i find love

--- king-vincent/src/main/java/frc/robot/subsystems/Climber.java
+++ king-vincent/src/main/java/frc/robot/subsystems/Climber.java
@@ -36,6 +36,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 
 public class Climber extends SubsystemBase {
